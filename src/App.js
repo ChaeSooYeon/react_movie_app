bf56df59a24e0d5b75794aba4685758589c79853
ChaeSooYeon <cotndus3@gmail.com>
2021-09-07 23:02:54 +0900

    map Recap ver.2 Use Key

--- src/App.js
+++ src/App.js
@@ -1,40 +1,52 @@
 import React from "react";
-
-function Food ({name, image}){
- return(
-   <div>
-     <h3>I like {name}</h3>
-     <img src={image} width="500px" />
-   </div>
- );
-}
+import PropTypes from "prop-types";
 
 const foodILike = [
   {
+    id:1,
     name:"kimchi",
-    image:"https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg"
+    image:"https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg",
+    rating:5
   },
   {
+    id:2,
     name:"samgyetang",
-    image:"https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg"
+    image:"https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg",
+    rating:4
   },
   {
+    id:3,
     name:"bulgogi",
-    image:"https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg"
+    image:"https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg",
+    rating:4.5
   },
   {
+    id:4,
     name:"kimbap",
-    image:"https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg"
+    image:"https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg",
+    rating:3.5
   },
 ]
 
+function Food ({name, image}){
+  return(
+    <div>
+      <h3>I like {name}</h3>
+      <img src={image} alt={name} width="500px" />
+    </div>
+  );
+ }
+
+function renderFood(dish){
+  console.log(dish); //-> keyerror
+  return <Food key={dish.id} name={dish.name} image={dish.image} />
+}
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-      {foodILike.map(dish => (
-        <Food name={dish.name} image={dish.image} />
-      ))}
+    <div>
+      {console.log(foodILike.map(renderFood))}
+      {foodILike.map(renderFood)}
     </div>
   );
 }
