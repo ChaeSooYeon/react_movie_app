8d047ad2cffa63de487092ba216c00d1d94f52fa
ChaeSooYeon <cotndus3@gmail.com>
2021-09-07 23:00:26 +0900

    map Recap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+function Food ({name, image}){
+ return(
+   <div>
+     <h3>I like {name}</h3>
+     <img src={image} width="500px" />
+   </div>
+ );
+}
+
+const foodILike = [
+  {
+    name:"kimchi",
+    image:"https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg"
+  },
+  {
+    name:"samgyetang",
+    image:"https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg"
+  },
+  {
+    name:"bulgogi",
+    image:"https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg"
+  },
+  {
+    name:"kimbap",
+    image:"https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg"
+  },
+]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
+      {foodILike.map(dish => (
+        <Food name={dish.name} image={dish.image} />
+      ))}
     </div>
   );
 }
