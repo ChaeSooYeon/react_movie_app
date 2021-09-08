75f81ab4f204917f065c7ed7eab7060db8201a2e
ChaeSooYeon <cotndus3@gmail.com>
2021-09-08 22:24:54 +0900

    #3.2 Component Life Cycle

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+// #3.2 Component Lifie Cycle 
+// component가 update될 때, 호출되는 다른 function들이 있다.
+// 1. mounting : 태어나는것
+//    - constructor() : constructor() 호출 후 render() 호출
+//    - render()
+//    - componentDidMount() : component가 처음 render 되면 호출
+// 2. updating : 일반적인 업데이트
+//    - render()
+//    - componentDidUpdate() : component가 update 되면 호출
+// 3. Unmounting : component가 죽는것 = page가 바뀔때
+//                ex) state를 사용해 component를 교체 
+//    - componentWillUnmount() 
+
+
 class App extends React.Component{
   state = {
     count: 0
@@ -13,8 +27,19 @@ add = () => {
 minus = () => {
   this.setState(nameOfFunction => ({count: nameOfFunction.count - 1}));
 };
-
+// *** 뇌에 새길것! ***
+// 매순간 setState를 호출할 때 마다 react는 새로운 state와 함께 render function을 호출한다
+componentDidMount(){
+  console.log("component rendered");
+} 
+componentDidUpdate(){
+  console.log("component updated");
+}
+componentWillUnmount(){
+  console.log("Goodbye, Component");
+}
   render(){
+    console.log("render is rendering");
     return( 
       <div>
         <h1>The number is: {this.state.count}</h1>
@@ -27,3 +52,4 @@ minus = () => {
 }
 
 export default App;
+
