0894f455c16791dea68ab8595055d38fb49dae2b
ChaeSooYeon <cotndus3@gmail.com>
2021-09-08 22:54:32 +0900

    add Interval counting

--- src/App.js
+++ src/App.js
@@ -4,17 +4,23 @@ import PropTypes from "prop-types";
 class App extends React.Component{
   state ={
     isLoading :true,
+    loadingCount : 1,
     movie: []
   };
   componentDidMount(){
     setTimeout(()=> {
       this.setState({isLoading: false});     
-    }, 6000);
+    }, 5000);
+    setInterval(() => {
+      if(this.state.loadingCount < 6)
+        this.setState(current => ({loadingCount : current.loadingCount + 1}));
+    }, 1000);
   }
   render(){
-    const {isLoading} = this.state;
+    const {isLoading, loadingCount} = this.state;
     return(
       <div>
+        <h4>{loadingCount}</h4>
        {isLoading ? "Loading..." : "We are ready"}
       </div>
     ); 
