25b3deca5ffb7d4a6526672b10c322c49d2b3471
ChaeSooYeon <cotndus3@gmail.com>
2021-09-11 00:06:48 +0900

    #6.2 Building the Navigation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React from "react";
-import { HashRouter, Route } from "react-router-dom";
+import { BrowserRouter, HashRouter, Route } from "react-router-dom";
 import About from "./routes/About";
 import Home from "./routes/Home";
- 
+import Navigation from "./componetns/Navigation";
 
 function App() {
   return (
-    <HashRouter>
-      <Route path="/" exact={true} component={Home} />
-      <Route path="/about" exact={true} component={About} />
-    </HashRouter>
+    <>
+      <HashRouter>
+        {/* // Link는 라우터 안에서 쓰여야하기 때문에 <Navigation/> 은 라우터 밖에서 선언할 수 없음 */}
+        <Navigation />
+        <Route path="/" exact={true} component={Home} />
+        <Route path="/about" component={About} />
+      </HashRouter>
+      {/* BrowserRouter : url에 해시(#)가 안붙음. But,github pages에 올리기에는 HashRouter가 쉬움 */}
+      {/* <BrowserRouter>
+        <Navigation />
+        <Route path="/" exact={true} component={Home} />
+        <Route path="/about" component={About} />
+      </BrowserRouter> */}
+    </>
   );
 }
 
